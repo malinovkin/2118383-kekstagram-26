8e286615ac00119951918b332e415af7cdc44e9e
malinovkin <mychest@list.ru>
2022-07-19 20:33:10 +0300

    "Рефакторинг" функции getRandomInteger

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 // Функция возвращает случайное целое число из переданного диапазона включительно.
 // Если входные данные не корректны, то функция возвращает -1
 function getRandomInteger(minValue, maxValue) {
-  if ((minValue >= 0) && (minValue < maxValue)) {
-    return Math.floor(minValue + Math.random() * (maxValue - minValue + 1));
-  } else {
-    return -1;
-  }
+  return ((minValue >= 0) && (minValue < maxValue)) ?
+    Math.floor(minValue + Math.random() * (maxValue - minValue + 1)) : -1;
 }
 
 // Функция для проверки максимальной длины строки.
