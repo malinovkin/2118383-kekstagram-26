a6a0d96f334fdb26314e36d0e777aa56d404ee80
malinovkin <mychest@list.ru>
2022-07-22 01:52:39 +0300

    Загрузка нового изображения на сайт

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 import {generateData} from './generate-data.js';
 
 generateData();
+
+const fileReader = new FileReader();
+const uploadFileButton = document.getElementById('upload-file');
+
+// функция выборки файла
+function loadImageFile() {
+  const file = uploadFileButton.files[0];
+  if (file) {
+    fileReader.readAsDataURL(file);
+  }
+}
+
+// функция закрытия редактора
+function closeEditor() {
+  document.querySelector('.img-upload__overlay').classList.add('hidden');
+  document.body.classList.remove('modal-open');
+}
+
+document.getElementById('upload-cancel').addEventListener('click', closeEditor);
+document.body.addEventListener('keydown', (e) => {
+  if (e.code === 'Escape') {
+    closeEditor();
+  }
+});
+
+uploadFileButton.addEventListener('change', loadImageFile, false);
+
+fileReader.onload = function(e) {
+  document.querySelector('.img-upload__preview img').src = e.target.result;
+  document.querySelector('.img-upload__overlay').classList.remove('hidden');
+  document.body.classList.add('modal-open');
+};
