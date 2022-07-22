ff23fb145b0c35f66349e14a52c7281d779b2051
malinovkin <mychest@list.ru>
2022-07-22 03:31:21 +0300

    Редактирование изображения и ограничения, накладываемые на поля

--- js/main.js
+++ js/main.js
@@ -2,34 +2,64 @@ import {generateData} from './generate-data.js';
 
 generateData();
 
-const fileReader = new FileReader();
+class ImageEditor {
+  constructor() {
+    this.setScale(100);
+    document.body.addEventListener('keydown', (e) => {
+      if (e.code === 'Escape') {
+        this.close();
+      }
+    });
+    document.getElementById('upload-cancel').addEventListener('click', () => { this.close(); }, false);
+    document.querySelector('.scale__control--smaller').addEventListener('click',
+      () => { this.setScale(this.scale - 25); }, false);
+    document.querySelector('.scale__control--bigger').addEventListener('click',
+      () => { this.setScale(this.scale + 25); }, false);
+  }
+
+  // закрытие редактора
+  close() {
+    document.querySelector('.img-upload__overlay').classList.add('hidden');
+    document.body.classList.remove('modal-open');
+  }
+
+  // открытие редактора
+  show(image) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.querySelector('.img-upload__preview img').src = e.target.result;
+      document.querySelector('.img-upload__overlay').classList.remove('hidden');
+      document.body.classList.add('modal-open');
+    };
+    if(image.files[0]) {
+      // загрузка изображения
+      reader.readAsDataURL(image.files[0]);
+    }
+    this.setScale(100);
+  }
+
+  // установка масштаба изображения
+  setScale(scale) {
+    if (scale < 0) {
+      this.scale = 0;
+    } else if (scale > 100) {
+      this.scale = 100;
+    } else {
+      this.scale = scale;
+    }
+    document.querySelector('input[name=scale]').value = `${this.scale}%`;
+    document.querySelector('.img-upload__preview img').style.transform = `scale(${this.scale/100})`;
+  }
+
+  // установка эффекта на изображение
+  setEffect(effectName, effecIntensity) {
+
+  }
+}
+
 const uploadFileButton = document.getElementById('upload-file');
+const imageEditor = new ImageEditor();
 
-// функция выборки файла
-function loadImageFile() {
-  const file = uploadFileButton.files[0];
-  if (file) {
-    fileReader.readAsDataURL(file);
-  }
-}
-
-// функция закрытия редактора
-function closeEditor() {
-  document.querySelector('.img-upload__overlay').classList.add('hidden');
-  document.body.classList.remove('modal-open');
-}
-
-document.getElementById('upload-cancel').addEventListener('click', closeEditor);
-document.body.addEventListener('keydown', (e) => {
-  if (e.code === 'Escape') {
-    closeEditor();
-  }
-});
-
-uploadFileButton.addEventListener('change', loadImageFile, false);
-
-fileReader.onload = function(e) {
-  document.querySelector('.img-upload__preview img').src = e.target.result;
-  document.querySelector('.img-upload__overlay').classList.remove('hidden');
-  document.body.classList.add('modal-open');
-};
+uploadFileButton.addEventListener('change', () => {
+  imageEditor.show(uploadFileButton);
+}, false);
